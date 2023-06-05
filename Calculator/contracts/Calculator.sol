bf5028c08784dd99b7b99f53a379d1e9b6e15da9
ChubbyIsaak <chubbyisaac908@gmail.com>
2023-06-05 01:19:11 -0600

    Añadido

--- Calculator/contracts/Calculator.sol
+++ Calculator/contracts/Calculator.sol
@@ -1,3 +1,4 @@
+// SPDX-License-Identifier: MIT 
 pragma solidity >=0.4.22 <0.9.0;
 
 contract Calculator {
@@ -5,7 +6,7 @@ contract Calculator {
   }
 
   // Add two numbers together
-  function add(uint256 a, uint256 b) public pure returns (uint256) {
+  function add(uint256 a, uint256 b) public pure  returns (uint256) {
     return a + b;
   }
 
--- Calculator/migrations/2_deploy_calculator.js
+++ Calculator/migrations/2_deploy_calculator.js
@@ -1,22 +1,11 @@
-const web3 = require("truffle/web3");
+const Calculator = artifacts.require("Calculator");
 
-
-class MyMigrations {
-    constructor(web3) {
-        this.web3 = web3;
-    }
-
-    async deploy() {
-        const calculator = await this.web3.eth.contract(
-            require("./contracts/Calculator.sol").Calculator
-        ).new({
-            from: web3.eth.accounts[0],
-            data: Calculator.code,
-            gas: 5000000,
+module.exports = function (deployer, network, accounts) {
+    deployer.deploy(Calculator)
+        .then(() => {
+            return Calculator.deployed();
+        })
+        .then((instance) => {
+            console.log("Calculator deployed to address:", instance.address);
         });
-
-        console.log("Calculator deployed to address:", calculator.address);
-    }
-}
-
-module.exports = new MyMigrations(web3);
+};
