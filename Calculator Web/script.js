65e5c83812579dfbea50c641c6913eb1771b0548
ChubbyIsaak <chubbyisaac908@gmail.com>
2023-06-07 14:06:33 -0600

    Modificado

--- Calculator Web/script.js
+++ Calculator Web/script.js
@@ -89,12 +89,12 @@ function add() {
     const number2 = parseInt(document.getElementById('number2').value);
 
     const options = {
-        from: '0x5692614fC1a969759a8F307c3847758E2Bf4E5e7' // Reemplaza '0xYourAddressHere' con tu dirección Ethereum válida
+        from: '0x5692614fC1a969759a8F307c3847758E2Bf4E5e7'
     };
 
     calculatorContract.methods.add(number1, number2).send(options)
         .then(() => {
-            document.getElementById('result').textContent = 'Addition successful';
+            document.getElementById('result').textContent = 'Suma exitosa';
         })
         .catch((error) => {
             document.getElementById('error').textContent = 'Error: ' + error.message;
@@ -106,12 +106,12 @@ function subtract() {
     const number2 = parseInt(document.getElementById('number2').value);
 
     const options = {
-        from: '0x48331AF1dF570C1d05E8230c0110644B284f7B1D' // Reemplaza '0xYourAddressHere' con tu dirección Ethereum válida
+        from: '0x48331AF1dF570C1d05E8230c0110644B284f7B1D'
     };
 
     calculatorContract.methods.subtract(number1, number2).send(options)
         .then(() => {
-            document.getElementById('result').textContent = 'Subtraction successful';
+            document.getElementById('result').textContent = 'Resta exitosa';
         })
         .catch((error) => {
             document.getElementById('error').textContent = 'Error: ' + error.message;
@@ -123,12 +123,12 @@ function multiply() {
     const number2 = parseInt(document.getElementById('number2').value);
 
     const options = {
-        from: '0x3d2fA460d3667D9d257cb066C8e77925a03C3c30' // Reemplaza '0xYourAddressHere' con tu dirección Ethereum válida
+        from: '0x3d2fA460d3667D9d257cb066C8e77925a03C3c30'
     };
 
     calculatorContract.methods.multiply(number1, number2).send(options)
         .then(() => {
-            document.getElementById('result').textContent = 'Multiplication successful';
+            document.getElementById('result').textContent = 'Multiplicacion Exitosa';
         })
         .catch((error) => {
             document.getElementById('error').textContent = 'Error: ' + error.message;
@@ -140,12 +140,12 @@ function divide() {
     const number2 = parseInt(document.getElementById('number2').value);
 
     const options = {
-        from: '0x847AA6f9F192d95FC57914D7a9b27d627D4B7654' // Reemplaza '0xYourAddressHere' con tu dirección Ethereum válida
+        from: '0x847AA6f9F192d95FC57914D7a9b27d627D4B7654'
     };
 
     calculatorContract.methods.divide(number1, number2).send(options)
         .then(() => {
-            document.getElementById('result').textContent = 'Division successful';
+            document.getElementById('result').textContent = 'Division exitosa';
         })
         .catch((error) => {
             document.getElementById('error').textContent = 'Error: ' + error.message;
